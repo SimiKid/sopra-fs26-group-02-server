1fe92ee168f1fe1ee64995241aa87b8eb088fe27
oliver <ohaas61@gmail.com>
2026-04-13 15:03:19 +0200

    fix: allow OPTIONS preflight requests in AuthInterceptor

--- src/main/java/ch/uzh/ifi/hase/soprafs26/Interceptor/AuthInterceptor.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/Interceptor/AuthInterceptor.java
@@ -18,6 +18,9 @@ public class AuthInterceptor implements HandlerInterceptor{
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler){
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+        return true;
+        }
         String token = request.getHeader("Authorization");
         authenticationService.authenticateByToken(token);
         return true;
